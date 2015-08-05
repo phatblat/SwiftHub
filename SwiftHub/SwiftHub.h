26b984e26cc5c934c65c353d2989efe15755781f
Ben Chatelain <benchatelain@gmail.com>
2015-08-04 20:36:41 -0600

    Initial Commit

---
+++ SwiftHub/SwiftHub.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHub.h
+//  SwiftHub
+//
+//  Created by Ben Chatelain on 8/4/15.
+//  Copyright © 2015 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftHub.
+FOUNDATION_EXPORT double SwiftHubVersionNumber;
+
+//! Project version string for SwiftHub.
+FOUNDATION_EXPORT const unsigned char SwiftHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHub/PublicHeader.h>
+
+
